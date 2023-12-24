fd4a358de388c6c36749f2f681c9330e95c8c65a
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2023-12-24 15:53:40 +0000

    Update pipeline

---
+++ .github/workflows/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+repo_name=${{ github.repository.full_name }}
+repo=$(echo $repo_name | tr "/" "-")
+branch=${{ github.ref_name }}
+run_id=${{ github.run_id }}
+
+echo $repo
+echo $branch 
+echo $run_id 
+
+curl -X POST \
+      -H "Content-Type: application/xml" \
+      -d "@./test-report.xml" \
+      https://d620-2001-818-d998-b00-e5d5-332d-f89-e699.ngrok.io/reports/$(echo $repo)/$(echo $branch)/$(echo $run_id)
