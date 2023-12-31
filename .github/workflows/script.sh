53258ef457f2b959e93c3132527a85b66305eeb7
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2023-12-31 12:45:36 +0000

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -7,5 +7,5 @@ current_run_id=${GITHUB_RUN_ID}
 data=$(cat ./test-report.xml | base64)
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d "{\"repo_name\":\"$current_repo\", \"branch\": \"$current_branch\", \"run_id\": \"$run_id\", \"report_body\": \"$data\"}" \
+  -d "{\"repo_name\":\"$current_repo\", \"branch\": \"$current_branch\", \"run_id\": \"$current_run_id\", \"report_body\": \"$data\"}" \
   https://113d-89-114-64-217.ngrok-free.app/reports
