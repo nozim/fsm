5d2f15efb9ae20799e4f6a598fc1e7624645e6e4
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2024-01-02 14:15:25 +0000

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -5,4 +5,4 @@ current_branch=${GITHUB_REF##*/}
 current_run_id=${GITHUB_RUN_ID}
 
 curl -X POST -H "Content-Type: application/xml" -H "RepoName: ${current_repo}" -H "Branch: ${current_branch}" -H "RunID: ${current_run_id}" -d @./test-report.xml \
-    https://419d-2001-818-d998-b00-746e-9cc2-1797-2219.ngrok-free.app/reports
+    http://api.codhero.co/reports
