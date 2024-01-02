25fda1cd49a72cf6554f0813d4e286ab61f70cb1
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2024-01-02 13:58:24 +0000

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,12 +4,5 @@ current_repo=${GITHUB_REPOSITORY##/}
 current_branch=${GITHUB_REF##*/}
 current_run_id=${GITHUB_RUN_ID}
 
-data=$(cat ./test-report.xml | base64)
-
-payload="{\"repo_name\":\"$current_repo\", \"branch\": \"$current_branch\", \"run_id\": \"$current_run_id\", \"report_body\": \"$data\"}"
-echo $payload
-
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d $payload \
-  https://419d-2001-818-d998-b00-746e-9cc2-1797-2219.ngrok-free.app/reports
+curl -X POST -H "Content-Type: application/xml" -H "RepoName: ${current_repo}" -H "Branch: ${current_branch}" -H "RunID: ${current_run_id}" -d @./test-report.xml \
+    https://419d-2001-818-d998-b00-746e-9cc2-1797-2219.ngrok-free.app/reports
