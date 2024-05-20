d830db475eca5fd66cc8d349c52def95c98b9a5a
m8vn3m <130057911+m8vn3m@users.noreply.github.com>
2024-05-20 18:17:31 +0100

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -6,4 +6,4 @@ current_run_id=${GITHUB_RUN_ID}
 current_commit=${GITHUB_SHA}
 
 curl -X POST -H "Content-Type: application/xml" -H "RepoName: ${current_repo}" -H "Branch: ${current_branch}" -H "RunID: ${current_run_id}" -H "CommitSHA: ${current_commit}" -d @./test-report.xml \
-    https://b8b8-2001-818-d998-b00-a1bf-3475-ddb1-bf4e.ngrok-free.app/reports
+    https://0e87-2001-818-d998-b00-a977-bd2f-a5b-e98d.ngrok-free.app/reports
