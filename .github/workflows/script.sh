0ebaafc984beeff61449c5442d92ebd3f04d7d0c
m8vn3m <130057911+m8vn3m@users.noreply.github.com>
2024-05-27 13:37:53 +0100

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -6,4 +6,4 @@ current_run_id=${GITHUB_RUN_ID}
 current_commit=${GITHUB_SHA}
 
 curl -X POST -H "Content-Type: application/xml" -H "RepoName: ${current_repo}" -H "Branch: ${current_branch}" -H "RunID: ${current_run_id}" -H "CommitSHA: ${current_commit}" -d @./test-report.xml \
-    https://api.codhero.co/reports
+    http://api.codhero.co/reports
